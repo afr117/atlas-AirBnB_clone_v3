f21f7c2fd4b6ecee945fa4a5ddb6e7c17608c05a
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-14 19:44:22 +0000

    connectiong issues modify app and index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-"""API status routes."""
+# api/v1/views/index.py
+
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
@@ -7,13 +7,17 @@ from models import storage
 
 @app_views.route('/api/v1/stats', methods=['GET'])
 def stats():
-    """Retrieve the number of each object by type."""
-    stats = {
+    """Retrieve the number of each object by type.
+    
+    Returns:
+        JSON: A dictionary containing the counts of various objects.
+    """
+    counts = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
-        "users": storage.count("User"),
+        "users": storage.count("User")
     }
-    return jsonify(stats)
+    return jsonify(counts)
