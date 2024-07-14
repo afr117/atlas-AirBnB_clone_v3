1f35d33180c823473f60ee85db987948f9d07824
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-14 19:53:16 +0000

    test2

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,21 +3,16 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os  # Import os module
 
 app = Flask(__name__)
-
-# Register the blueprint for API routes
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Teardown function that closes the database session."""
+def teardown_appcontext(exception):
+    """Close the storage on teardown."""
     storage.close()
 
 if __name__ == "__main__":
-    # Run the Flask server
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', 5000)),
             threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,6 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
-
 @app_views.route('/api/v1/stats', methods=['GET'])
 def stats():
     """Retrieve the number of each object by type."""
