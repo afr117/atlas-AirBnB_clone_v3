26ebeae020badde26dc57e10e20fc1564408a77c
BradBrown6621 <6621@holbertonstudents.com>
2024-07-16 16:05:03 -0500

    api/v1/app.py: task 6: Implemented rudimentary error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ api/v1/app.py
 """
 
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -17,7 +17,14 @@ def teardown_appcontext(exception):
     """Close the storage on teardown."""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(e):
+    return make_response(jsonify({
+        "error": "Not found"
+        }), 404)
+
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', 5000)),
             threaded=True)
+
