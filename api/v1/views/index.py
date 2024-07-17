855c11b02eb774873fead56a69f127056ed781a4
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-17 16:43:21 +0000

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,27 +1,10 @@
-#!usr/bin/python3
-"""
-api/v1/views/index.py
-"""
+#!/usr/bin/python3
 
 from flask import jsonify
 from api.v1.views import app_views
-from models import storage
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    status = {
-            "status": "OK"
-            }
-    return jsonify(status)
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
 
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    """Retrieve the number of each object by type."""
-    return jsonify({
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    })
