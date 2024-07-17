5b676faab442cc118651f3d5420b406866cbfe17
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-17 17:12:54 +0000

    pep 8 t4 correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
+
 import os  # Ensure os is imported
 
 
@@ -16,12 +17,12 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 
-
 def teardown(exception):
     """Closes the storage on teardown."""
     storage.close()
 
 if __name__ == "__main__":
+
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', 5000)),
             threaded=True)
