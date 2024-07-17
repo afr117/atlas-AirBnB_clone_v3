a88f24e07947400f5bab8c5f68b8a95cbb07c0a1
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-17 16:46:53 +0000

    documentationt4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
+"""
+app.py - Flask application for the HBNB API.
+"""
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
+import os  # Ensure os is imported
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Close storage on teardown"""
+    """Closes the storage on teardown."""
     storage.close()
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
-
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', 5000)),
+            threaded=True)
