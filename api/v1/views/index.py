2cfb4e9915826c3b2b9d4bbccbc98ba3f7f3adea
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-17 17:16:58 +0000

    pep 8 index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ index.py - API views for handling status endpoint.
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns the status of the API."""
