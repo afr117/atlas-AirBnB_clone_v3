99873a65e300e423b6097517ea975f0013486bd0
afr117 <54492982+afr117@users.noreply.github.com>
2024-07-18 17:16:43 -0400

    Update __init__.py
    init updated for t12

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,4 +5,5 @@ from api.v1.views import cities
 from api.v1.views import states
 from api.v1.views import amenities
 from api.v1.views import users
-from api.v1.views import places  # Add this line
+from api.v1.views import places
+from api.v1.views import places_reviews  # Add this line
