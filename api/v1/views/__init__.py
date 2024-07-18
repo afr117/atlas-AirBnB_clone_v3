6202342aba1e87a81d4a728ee4ad6a28cc416e0a
afr117 <54492982+afr117@users.noreply.github.com>
2024-07-18 17:05:27 -0400

    Update __init__.py
    init replace

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,7 @@
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views import (cities, states, amenities)  # Import the new file here
+from api.v1.views import cities
+from api.v1.views import states
+from api.v1.views import amenities
+from api.v1.views import users  # Add this line
