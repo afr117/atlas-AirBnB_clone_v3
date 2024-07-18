0e4c4e0626b94806acb7523d754ed8d2a12a55f1
afr117 <54492982+afr117@users.noreply.github.com>
2024-07-18 17:11:23 -0400

    Update __init__.py
    init upd

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,4 +4,5 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views import cities
 from api.v1.views import states
 from api.v1.views import amenities
-from api.v1.views import users  # Add this line
+from api.v1.views import users
+from api.v1.views import places  # Add this line
