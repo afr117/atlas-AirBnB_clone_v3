9626c289a9addff314b9bd57db32a9a1c43669f6
afr117 <54492982+afr117@users.noreply.github.com>
2024-07-18 18:20:33 -0400

    Update amenities.py
    pep8 amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,13 +5,17 @@ from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
 
-@app_views.route('/amenities', strict_slashes=False, methods=['GET'])
+
+@app_views.route('/amenities',
+                 strict_slashes=False, methods=['GET'])
 def get_amenities():
     """Retrieve a list of all Amenity objects"""
     amenities = storage.all(Amenity)
     return jsonify([amenity.to_dict() for amenity in amenities.values()])
 
-@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['GET'])
+
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False, methods=['GET'])
 def get_amenity(amenity_id):
     """Retrieve an Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -19,7 +23,9 @@ def get_amenity(amenity_id):
         abort(404)
     return jsonify(amenity.to_dict())
 
-@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['DELETE'])
+
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False, methods=['DELETE'])
 def delete_amenity(amenity_id):
     """Delete an Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -39,14 +45,15 @@ def create_amenity():
     data = request.get_json()
     if 'name' not in data:
         return make_response(jsonify({"error": "Missing name"}), 400)
-    
+
     new_amenity = Amenity(**data)
     storage.new(new_amenity)
     storage.save()
     return make_response(jsonify(new_amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['PUT'])
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False, methods=['PUT'])
 def update_amenity(amenity_id):
     """Update an existing Amenity object"""
     if not request.is_json:
