683b91c7514788decc007a5ad43d06f6c2d30f3c
afr117 <54492982+afr117@users.noreply.github.com>
2024-07-18 15:19:49 -0400

    Update app.py
    added return error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,26 +3,25 @@
 app.py - Flask application for the HBNB API.
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-
-
-import os  # Ensure os is imported
-
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
 
 @app.teardown_appcontext
 def teardown(exception):
     """Closes the storage on teardown."""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors."""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', 5000)),
             threaded=True)
