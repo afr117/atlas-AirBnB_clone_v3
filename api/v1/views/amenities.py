56da47dbcef9eb9c95991e63a88d2e79622d788e
afr117 <54492982+afr117@users.noreply.github.com>
2024-07-18 16:51:38 -0400

    Update amenities.py
    def creat amenities updated

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -29,12 +29,14 @@ def delete_amenity(amenity_id):
     storage.save()
     return {}, 200
 
+
 @app_views.route('/amenities', strict_slashes=False, methods=['POST'])
 def create_amenity():
     """Create a new Amenity object"""
-    data = request.get_json()
-    if not data:
+    if not request.is_json:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
+
+    data = request.get_json()
     if 'name' not in data:
         return make_response(jsonify({"error": "Missing name"}), 400)
     
@@ -42,6 +44,7 @@ def create_amenity():
     storage.new(new_amenity)
     storage.save()
     return make_response(jsonify(new_amenity.to_dict()), 201)
+
 
 @app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['PUT'])
 def update_amenity(amenity_id):
